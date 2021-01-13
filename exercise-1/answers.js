3b7f8b642a9778f26218143225e5d18930181d23
Tanin Thanasopon <t.thanasopon@gmail.com>
2021-01-13 17:03:35 +0700

    first commit

---
+++ exercise-1/answers.js
@@ -0,0 +1,28 @@
+function sayHello () {
+    // TODO: return คำว่า "Hello world!"
+}
+
+function isString (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น String ให้ return true, ถ้าไม่ใช่ return false
+}
+
+function isNumber (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Number ให้ return true, ถ้าไม่ใช่ return false
+}
+
+function isArray (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Array ให้ return true, ถ้าไม่ใช่ return false
+}
+
+function isObject (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Object ให้ return true, ถ้าไม่ใช่ return false
+}
+
+function isFunction (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Function ให้ return true, ถ้าไม่ใช่ return false
+}
---
+++ exercise-2/answers.js
@@ -0,0 +1,23 @@
+function convertADtoBE(input) {
+    // AD คือคริสดศักราย
+    // BE คือพุทธศักราช
+    //
+    // TODO: แปลงปีใน พุทธศักราช เป็น คริสตศักราช เช่น 2000 เป็น "พ.ศ. 2543"
+    // โดยให้เติมตัวอักษร พ.ศ. เข้าไปด้านหน้าด้วย
+}
+
+function evenOrOdd(input) {
+    // TODO: ให้ตรวจสอบว่าตัวเลข input เป็นเลขคู่หรือเลขคี่
+}
+
+function getFullName(input) {
+    // TODO: ให้นำคำนำหน้าชื่อ ชื่อต้น นามสกุล มาต่อกัน
+}
+
+function getFirstName(input) {
+    // TODO: ให้ทำการตัดนามสกุลออกโดยใช้ indexOf() และ substring()
+}
+
+function splitName(input) {
+    // TODO: ให้ทำการแยก ชื่อต้นกับนามสกุล
+}
